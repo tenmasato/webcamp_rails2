3455f747358cae67ed701a8569c84aad9273b8ff
tenmasato <narito1015@icloud.com>
2021-11-23 08:07:30 +0000

    [Add]投稿一覧

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -4,13 +4,14 @@ class PostImagesController < ApplicationController
   end
 
   def create
-    @post_image = PostImage.find(post_image_params)
+    @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_image_path
+    redirect_to post_images_path
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
@@ -22,6 +23,6 @@ class PostImagesController < ApplicationController
   private
 
   def post_image_params
-    parmas.require(:post_image).permit(:shop_name,:image,:caption)
+    params.require(:post_image).permit(:shop_name,:image,:caption)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '74d0413d9c78645d0e286c38a17fb419192449ed7f241c4def14cbc1286a6e5694e99cf8c731afcccd206a957d85bb1038c762658ea142a00348966d614377d2'
