61e725b4bc04246d2b4fe78cc3ce35c78602076f
tenmasato <narito1015@icloud.com>
2021-11-23 08:30:09 +0000

    [Update]ログイン後遷移先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+    def after_sign_in_path_for(resoure)
+      post_images_path
+    end
 
     protected
 
